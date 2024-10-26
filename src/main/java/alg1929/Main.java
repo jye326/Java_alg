c04b19ccd8d6c320448c28396f1bb6f9e70ed0b1
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-10-26 15:09:30 +0900

    solved  - learned   - 배수 필터링   - 에라토스테네스의 체

---
+++ src/main/java/alg1929/Main.java
@@ -0,0 +1,33 @@
+package alg1929;
+
+import java.util.Scanner;
+
+// 소수 구하기
+// M이상 N이하의 소수를 모두 출력
+// 에라토스테네스의 체
+public class Main {
+    public static void main(String[] args) {
+        boolean[] table = new boolean[1000001];
+        for (int i = 2; i < 1000001; i++){
+            table[i] = true;
+        }
+        for (int i = 2;i<1000001;i++) {
+            if (table[i]) {
+                for (int j = 2;i*j<1000001;j++) {
+                    table[i*j] = false;
+                }
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        for (int i = M; i<=N;i++){
+            if (table[i]) {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
